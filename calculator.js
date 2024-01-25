1c3695c2cfe69aac1489d6b415929eaf3b4d08f0
paanncem <157663490+paanncem@users.noreply.github.com>
2024-01-25 11:38:50 +0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Conversion Process</title>
+</head>
+<body>
+    <header>
+        <h1>Conversion Processes</h1>
+    </header>
+    <section>
+        <p>This page will describe various conversion processes such as currency conversion, measurement conversion, character conversion, and more. A conversion calculator also a tool or application that allows users to convert values from unit or system to another. Enjoy you calculator </p>
+    </section>
+    <footer>
+        <p>&copy; 2024 [adib farhan<3]</p>
+    </footer>
+</body>
+</html>
+
+
